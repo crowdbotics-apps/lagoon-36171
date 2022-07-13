1d398f50b9f74bdde56ed15cc5fe5a3ada6c5294
Crowdbotics <team@crowdbotics.com>
2022-07-13 04:02:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "lagoon_36171",
+  url: "https://lagoon_36171.botics.co",
+  api: "https://lagoon_36171.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
